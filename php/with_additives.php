080bad19c9a32e49ce4d90f4620fac5b8ce2ae84
Kryptopay <info@kryptopay.pl>
2015-02-20 12:32:48 +0100

    Update with_additives.php

--- php/with_additives.php
+++ php/with_additives.php
@@ -37,7 +37,7 @@ $url ='https://mystore.com?invoice_id=999999&status=ok';
 $url = urlencode($url);
 $url_fail ='https://mystore.com?invoice_id=999999&status=fail'; 
 $url_fail = urlencode($url_fail);
-$json_url="https://kryptopay.pl/api/create/index.php?api_public_key=$api_public_key&api_private_key=$api_private_key&amount=$amount&order_nr=$order_nr&description=$description&company=$company&name=$name&surname=$surname&address=$address&url=$url&url_fail=$url_fail";
+$json_url="https://kryptopay.pl/api/create/?api_public_key=$api_public_key&api_private_key=$api_private_key&amount=$amount&order_nr=$order_nr&description=$description&company=$company&name=$name&surname=$surname&address=$address&url=$url&url_fail=$url_fail";
 $json_data = file_get_contents($json_url);
 $json_feed = json_decode($json_data);
 $transaction=$json_feed->transaction;
